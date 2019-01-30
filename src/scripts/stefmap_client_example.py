e31675d46dab0c79d582212ba6aa95714e68d3a0
sergimolina <sergimolina91@gmail.com>
2019-01-30 09:44:40 +0000

    added client example

---
+++ src/scripts/stefmap_client_example.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import sys
+import rospy
+from stefmap_ros.srv import GetSTeFMap
+
+def add_two_ints_client(x, y):
+    rospy.wait_for_service('add_two_ints')
+    try:
+        add_two_ints = rospy.ServiceProxy('add_two_ints', AddTwoInts)
+        resp1 = add_two_ints(x, y)
+        return resp1.sum
+    except rospy.ServiceException, e:
+        print "Service call failed: %s"%e
+
+
+if __name__ == "__main__":
+    prediction_time = 1352265000
+    order = 2
+    x_min = -45
+    x_max = 55
+    y_min = -35
+    y_max = 30
+    cell_size = 1
+
+    print "Requesting STeF-Map with the following parameters:\n"
+    print "  Time to predict: %s"%(prediction_time)
+    print "  Fremen Order: %s"%(order)
+    print "  X min: %s"%(x_min)
+    print "  X max: %s"%(x_max)
+    print "  Y min: %s"%(y_min)
+    print "  Y max: %s"%(y_max)
+    print "  Cell size:%s"%(cell_size)
+
+    rospy.wait_for_service('get_stefmap')
+    try:
+        get_stefmap = rospy.ServiceProxy('get_stefmap', GetSTeFMap)
+        stefmap = get_stefmap(prediction_time,order,x_min,x_max,y_min,y_max,cell_size)
+        print stefmap
+    
+    except rospy.ServiceException, e:
+        print "Service call failed: %s"%e 
+
